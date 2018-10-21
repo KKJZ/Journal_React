eb87b736ed7c467b355e9f1174d079080784e69e
Kyle Stockmann <kyle_stockmann@yahoo.com>
2018-10-21 11:23:22 -0700

    Welcome loading

--- journal/src/components/User_Page/main.js
+++ journal/src/components/User_Page/main.js
@@ -1,19 +1,30 @@
 import React from "react";
+import {connect} from 'react-redux';
 
-export default function Main(props) {
+export function Main(props) {
+	console.log(props)
+	let Title = props.entry.title;
+	let __Date = new Date.toString;
+	let Content = props.entry.content;
 	return (
 		<div className="user_page" role="User Page"
 		// style="background: white"
 		>
-			<h1 className="title">title</h1>
-			<h2 className="date">date</h2>
+			<h1 className="title">{Title}</h1>
+			<h2 className="date">{__Date}</h2>
 			<section className="content"
 			// style="background: white"
 			>
-			
+			{Content}
 			</section>
 			<button className="button is-success edit">Edit</button>
 			<button className="button is-danger del">Delete</button>
 		</div>
 	)
 }
+
+const mapStateToProps = state => ({
+	entry: state.profile.entry
+})
+
+export default connect(mapStateToProps)(Main)
